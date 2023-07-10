7c3fab697147ce34b2db2cab3326092372ffae51
Chirag Kalwani <75169388+chirag-kalwani@users.noreply.github.com>
2023-07-11 04:33:29 +0530

    logout problem resolved2

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -4,14 +4,13 @@ import {cookies} from "next/headers";
 
 export async function GET() {
     try {
-        cookies().set('token', '', {httpOnly: true});
+        cookies()?.set('token', '', {httpOnly: true});
         return NextResponse.json({
             msg: "Logout Success",
             success: true,
         }, {status: 200,});
-        ;
     } catch (error: any) {
-        NextResponse.json({
+        return NextResponse.json({
             error: error.message,
         }, {
             status: 500,
